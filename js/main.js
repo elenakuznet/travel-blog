908165ea95608768294679069f2e63521b15613a
Kuznetsova Elena <kuzn.elena@yahoo.com>
2024-06-30 12:35:53 +0500

    init: add header section

--- js/main.js
+++ js/main.js
@@ -1,7 +1,34 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+
+/* Menu show */
+if(navToggle) {
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* Menu hidden */
+if(navClose) {
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 
 /*=============== REMOVE MENU MOBILE ===============*/
+
+const navLinks = document.querySelectorAll('.nav__link');
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu');
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu');
+}
+
+navLinks.forEach((link) => link.addEventListener('click', linkAction));
 
 
 /*=============== ADD BLUR TO HEADER ===============*/
