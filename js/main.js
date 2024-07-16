037589d9eb537d40a38723f0f29fea4613111758
Kuznetsova Elena <kuzn.elena@yahoo.com>
2024-07-16 15:39:23 +0500

    add active link style

--- js/main.js
+++ js/main.js
@@ -33,7 +33,7 @@ navLinks.forEach((link) => link.addEventListener('click', linkAction));
 
 /*=============== ADD BLUR TO HEADER ===============*/
 
-const blurHeader = () =>{
+const blurHeader = () => {
     const header = document.getElementById('header')
     // Add a class if the bottom offset is greater than 50 of the viewport
     this.scrollY >= 50 ? header.classList.add('blur-header') 
@@ -43,8 +43,37 @@ window.addEventListener('scroll', blurHeader)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up');
+    //When the scroll is higher than 350 viewport hight, add the show scroll tag
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll');
+}
+
+window.addEventListener('scroll', scrollUp);
+
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
+
+	sections.forEach(current => {
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
